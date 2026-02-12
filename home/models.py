8c64226cc53851c4dc2ed6df3a286d620042fa5d
MuhammedNasihK <nasih8089@gmail.com>
2026-02-12 20:50:24 +0530

    Make some changes on models.py, changed the schema and field of tables

--- home/models.py
+++ home/models.py
@@ -21,54 +21,57 @@ class Brand(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True,max_length=250)
-    description = models.TextField()
-    specification = models.TextField()
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
     brand = models.ForeignKey(Brand,on_delete=models.CASCADE)
     added_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
     
-class ProductImages(models.Model):
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_image/')
-    
-
-class ProductVariant(models.Model):
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    variant_name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=12,decimal_places=2)
-    discount_price = models.DecimalField(max_digits=12,decimal_places=2,blank=True,null=True)
-    stock = models.PositiveIntegerField()
-
-    def __str__(self):
-        return f"{self.product.name}-{self.variant_name}"
-
 
 class Attribute(models.Model):
-    name = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
     
 
-
 class AttributeValue(models.Model):
     attribute = models.ForeignKey(Attribute,on_delete=models.CASCADE)
-    attribute_value = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.attribute.name} : {self.attribute_value}"
+        return f"{self.attribute} - {self.value}"
+    
 
-class VariantAttribute(models.Model):
-    product_variant = models.ForeignKey(ProductVariant,on_delete=models.CASCADE)
-    attribute_value = models.ForeignKey(AttributeValue,on_delete=models.CASCADE)
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product,on_delete = models.CASCADE)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    attribute = models.ManyToManyField(AttributeValue,related_name='varients')
 
     def __str__(self):
-        return f"{self.product_variant} - {self.attribute_value}"
+        return f"{self.product} - {self.attribute}"
+    
+
+class Specification(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    spec = models.CharField(max_length=200)
+    value = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.spec} - {self.value}"
+    
+
+class ProductImage(models.Model):
+    variant = models.ForeignKey(ProductVariant,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_image/')
+
+
+
 
 
 
