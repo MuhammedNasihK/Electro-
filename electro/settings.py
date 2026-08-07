66fe794f2fae4dd7e6dc44781914efc91ad43863
MuhammedNasihK <nasih8089@gmail.com>
2026-08-08 00:00:14 +0530

    Installed Razorpay and added on settings.py

--- electro/settings.py
+++ electro/settings.py
@@ -220,3 +220,7 @@ SOCIALACCOUNT_ADAPTER = 'accounts.adapters.CustomSocialAdapter'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID')
+RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET')
